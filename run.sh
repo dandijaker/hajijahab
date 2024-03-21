a9b13fe02af534044fa71fdd9d613ae70bc3cb8f
dandijaker <74192956+dandijaker@users.noreply.github.com>
2024-03-22 04:28:36 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-26628269-5455-4f57-84c0-089f8fd98c87/)
 
 
 echo "$curl_output" >> Update.tsx
